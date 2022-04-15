da25936fffce985400ad81afc48dd6927aa2cf3c
Brian Leonard <brian.leonard@mongodb.com>
2022-04-15 16:28:08 -0400

    Added transaction support

--- generate_user_profiles.py
+++ generate_user_profiles.py
@@ -1,93 +1,217 @@
+from tokenize import group
 import params
 import sys
 import time
 from datetime import datetime
+import pymongo
 from pymongo import MongoClient
 from faker import Faker
 import argparse
+import json
 from business_objects import *
 
-# Process arguments
-#parser = argparse.ArgumentParser(description='MongoDB Network Compression Test')
-# parser.add_argument('-c', '--compressor', help="The compressor to use.",
-#    choices=["snappy", 'zlib', 'zstd'])
-#args = parser.parse_args()
 
-print("\nGenerating Offers...")
+def get_collection(collection):
 
-# Establish connection to MongoDB
-
-# https://pymongo.readthedocs.io/en/stable/api/pymongo/mongo_client.html?highlight=compression#pymongo.mongo_client.MongoClient
-client = MongoClient(params.target_conn_string, compressors=params.compressor)
-
-print("Now:", datetime.now(), "\n")
-
-db = client[params.target_database]
-collection = db[params.target_collection]
-
-if params.drop_collection:
-    collection.drop()
-
-fake = Faker()
-records_to_insert = params.records_to_insert
-insert_count = 0  # Count the records inserted.
-batch_size = params.batch_size
-t_start = time.time()
-
-records = []  # array for bulk insert
-
-print("Records to insert: {},".format(records_to_insert))
-print("Batch insert size: {},".format(batch_size))
-
-groups = [
-    {'Id': 'A', 'Label': 'Alpha'},
-    {'Id': 'B', 'Label': 'Bravo'},
-    {'Id': 'C', 'Label': 'Charlie'},
-    {'Id': 'D', 'Label': 'Delta'},
-    {'Id': 'E', 'Label': 'Echo'}]
-
-while insert_count < records_to_insert:
-
-    groups_value = []
-
-    for x in range(fake.random_digit_not_null()):
-        group = fake.random_element(groups)
-        if group not in groups_value:   # Don't add the same group more than once
-            groups_value.append(fake.random_element(groups))
-
-    try:
-
-        record = {
-            "name": fake.name(),
-            "email": fake.ascii_email(),
-            "group": groups_value
-        }
-
-        records.append(record)
-
-        insert_count += 1
-
-        # If the modulus falls in the range of the record size, insert the batch.
-        if(insert_count % batch_size == 0):
-
-            collection.insert_many(records)
-            records = []
-
-            # Print performance stats
-            duration = time.time()-t_start
-            print('{:.0f}, records inserted'.format(insert_count),
-                  'at {:.1f}, records/second'.format(insert_count/duration))
-
-    except KeyboardInterrupt:
-        print
-        sys.exit(0)
-
-    except Exception as e:
-        print('\n********\nConnection Problem:')
-        print(e)
-        print('********')
-        sys.exit(0)
+    # Establish connection to MongoDB
+    # https://pymongo.readthedocs.io/en/stable/api/pymongo/mongo_client.html?highlight=compression#pymongo.mongo_client.MongoClient
+    client = MongoClient(params.target_conn_string,
+                         compressors=params.compressor)
+    db = client[params.target_database]
+    return db[collection]
 
 
-print("\n", insert_count, 'records inserted in',
-      str(round(time.time()-t_start, 0)), 'seconds')
+def get_group_document(groupId):
+    collection = get_collection(params.group_collection)
+    return collection.find_one({'Id': groupId}, {'_id': 0})
+
+
+def process_records(groups_collection, target_collection):
+
+    print("\nGenerating Records...")
+    print("Now:", datetime.now(), "\n")
+
+    if params.drop_collection:
+        target_collection.drop()
+
+    fake = Faker()
+    records_to_insert = params.records_to_insert
+    insert_count = 0  # Count the records inserted.
+    batch_size = params.batch_size
+    t_start = time.time()
+
+    records = []  # array for bulk insert
+
+    print("Records to insert: {},".format(records_to_insert))
+    print("Batch insert size: {},".format(batch_size))
+
+    groups = [
+        {'Id': 'A', 'Label': 'Alpha'},
+        {'Id': 'B', 'Label': 'Bravo'},
+        {'Id': 'C', 'Label': 'Charlie'},
+        {'Id': 'D', 'Label': 'Delta'},
+        {'Id': 'E', 'Label': 'Echo'}]
+
+    groups_collection.drop()
+    groups_collection.insert_many(groups)
+
+    while insert_count < records_to_insert:
+
+        groups_value = []
+
+        for x in range(fake.random_digit_not_null()):
+            group = fake.random_element(groups)
+            if group not in groups_value:   # Don't add the same group more than once
+                groups_value.append(fake.random_element(groups))
+
+        try:
+
+            record = {
+                "name": fake.name(),
+                "email": fake.ascii_email(),
+                "group": groups_value
+            }
+
+            records.append(record)
+
+            insert_count += 1
+
+            # If the modulus falls in the range of the record size, insert the batch.
+            if(insert_count % batch_size == 0):
+
+                target_collection.insert_many(records)
+                records = []
+
+                # Print performance stats
+                duration = time.time()-t_start
+                print('{:.0f}, records inserted'.format(insert_count),
+                      'at {:.1f}, records/second'.format(insert_count/duration))
+
+        except KeyboardInterrupt:
+            print
+            sys.exit(0)
+
+        except Exception as e:
+            print('\n********\nConnection Problem:')
+            print(e)
+            print('********')
+            sys.exit(0)
+
+    print("\n", insert_count, 'records inserted in',
+          str(round(time.time()-t_start, 0)), 'seconds')
+
+
+def group_size(collection, group):
+    count = collection.count_documents({'group.Id': group})
+    return count
+
+def commit_with_retry(session):
+    while True:
+        try:
+            # Commit uses write concern set at transaction start.
+            session.commit_transaction()
+            print("Transaction committed.")
+            break
+        except (pymongo.errors.ConnectionFailure, pymongo.errors.OperationFailure) as exc:
+            # Can retry commit
+            if exc.has_error_label("UnknownTransactionCommitResult"):
+                print("UnknownTransactionCommitResult, retrying "
+                      "commit operation ...")
+                continue
+            else:
+                print("Error during commit ...")
+                raise
+
+def delete_group(session, groups_collection, users_collection, group):
+    group_document = get_group_document(group)
+
+    print('Deleting {:,} members'.format(count), 'from group', group)
+    print(group_document)
+
+    print("Now:", datetime.now(), "\n")
+    t_start = time.time()
+
+    while True:
+        try:
+            with session.start_transaction():
+
+                # See Remove Items from an Array of Documents
+                # https://www.mongodb.com/docs/manual/reference/operator/update/pull/#remove-items-from-an-array-of-documents
+                result = users_collection.update_many({}, {'$pull': {'group': group_document}}, upsert=False, session=session)
+
+                print('{:,} user profiles updated in'.format(result.modified_count),
+                    str(round(time.time()-t_start, 0)), 'seconds')
+
+                result = groups_collection.delete_one({'Id': group}, upsert=False, session=session)
+                print(result.modified_count, 'group deleted from', params.groups_collection)
+
+                commit_with_retry(session)
+            break
+        except (pymongo.errors.ConnectionFailure, pymongo.errors.OperationFailure) as exc:
+            # If transient error, retry the whole transaction
+            if exc.has_error_label("TransientTransactionError"):
+                print("TransientTransactionError, retrying "
+                      "transaction ...")
+                continue
+            else:
+                raise
+
+
+
+
+def add_group(groups_collection, users_collection, group_document):
+
+    print('Adding', group_document, '...')
+
+    groups_collection.insert_one(group_document)
+
+    # Using existing group A as a cheap way to add a new group.
+    users_collection.update_many(
+        {'group.Id': 'A'}, {'$push': {'group': group_document}})
+
+
+if __name__ == "__main__":
+
+    # Process arguments
+    parser = argparse.ArgumentParser(description='MongoDB Large Transaction Test')
+    parser.add_argument('-t', '--task', help="The task to run",
+                    choices=['load', 'group_size', 'add_group', 'delete_group'])
+    parser.add_argument('-g', '--group', help="The group Id")
+    args = parser.parse_args()
+
+    # Establish connection to MongoDB
+    # https://pymongo.readthedocs.io/en/stable/api/pymongo/mongo_client.html?highlight=compression#pymongo.mongo_client.MongoClient
+    client = MongoClient(params.target_conn_string, compressors=params.compressor)
+    database = client[params.target_database]
+    user_profiles_collection = database[params.target_collection]
+    groups_collection = database[params.group_collection]
+
+    
+    if (args.task == 'load'):
+        process_records(groups_collection, user_profiles_collection)
+
+    # Return the count of group members
+    # python3 generate_user_profiles.py -t group_size -g A
+    elif (args.task == 'group_size'):
+        count = group_size(user_profiles_collection, args.group)
+
+        print('Group', args.group, 'has {:,} members'.format(count))
+
+    # The -g arg should be the full group document. E.g., {"Id": "E", "Label": "Echo"}
+    # python3 generate_user_profiles.py -t add_group -g '{"Id": "E", "Label": "Echo"}'
+    elif (args.task == 'add_group'):
+        group = json.loads(args.group)
+        add_group(groups_collection, user_profiles_collection, group)
+
+    # The -g arg should be the groupId. E.g. E
+    # python3 generate_user_profiles.py -t delete_group -g E
+    elif (args.task == 'delete_group'):
+        count = group_size(user_profiles_collection, args.group)
+
+        # Start a client session.
+        with client.start_session() as session:
+            # Use with_transaction to start a transaction, execute the callback, and commit (or abort on error).
+            delete_group(session, groups_collection, user_profiles_collection, args.group)
+
+    else:
+        print("usage: generate_user_profiles.py [-h] [-t {load,group_size,delete_group}]")
